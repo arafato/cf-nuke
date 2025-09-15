fccc82c3b81f0f6f732003db38801e89a9a28ad4
arafato <arafato@outlook.com>
2025-09-15 17:38:14 +0200

    added initial version

---
+++ resources/kv.go
@@ -0,0 +1,60 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cloudflare/cloudflare-go/v3"
+	"github.com/cloudflare/cloudflare-go/v3/kv"
+	"github.com/cloudflare/cloudflare-go/v3/option"
+
+	"github.com/arafato/cf-nuke/infrastructure"
+	"github.com/arafato/cf-nuke/types"
+)
+
+func init() {
+	infrastructure.RegisterCollector("kv", CollectKV)
+}
+
+type KV struct {
+	Client *kv.KVService
+}
+
+func CollectKV(creds *types.Credentials) error {
+	client := cloudflare.NewClient(
+		option.WithAPIToken(creds.APIKey),
+	)
+
+	fmt.Println("Collecting KV resources...")
+	page, err := client.KV.Namespaces.List(context.TODO(), kv.NamespaceListParams{
+		AccountID: cloudflare.F(creds.AccountID),
+	})
+
+	var allKVs []kv.Namespace
+
+	for len(page.Result) != 0 {
+		allKVs = append(allKVs, page.Result...)
+		page, err = page.GetNextPage()
+	}
+	if err != nil {
+		return err
+	}
+	fmt.Println("Finished Collecting KV resources...")
+
+	for _, kv := range allKVs {
+		res := types.Resource{
+			Removable:    KV{Client: client.KV},
+			ResourceID:   kv.ID,
+			ResourceName: kv.Title,
+			AccountID:    creds.AccountID,
+			ProductName:  "KV",
+		}
+		infrastructure.CollectResource(&res)
+	}
+
+	return nil
+}
+
+func (q KV) Remove(accountID string, resourceID string) error {
+	return nil
+}
