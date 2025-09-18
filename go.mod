8b209bbf3c16a05ecfd3b8ddaed790f015ed43bf
arafato <arafato@outlook.com>
2025-09-18 09:31:52 +0200

    pretty printing with github.com/olekukonko/tablewriter

--- go.mod
+++ go.mod
@@ -10,11 +10,20 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v1.1.0 // indirect
+	github.com/olekukonko/ll v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v1.0.9 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- utils/printer.go
+++ utils/printer.go
@@ -3,8 +3,11 @@ package utils
 import (
 	"context"
 	"fmt"
+	"os"
 	"sync"
 	"time"
+
+	"github.com/olekukonko/tablewriter"
 
 	"github.com/arafato/cf-nuke/types"
 )
@@ -26,9 +29,21 @@ func PrintStatusWithContext(wg *sync.WaitGroup, ctx context.Context, resources t
 }
 
 func PrettyPrintStatus(resources types.Resources) {
-	fmt.Print("\n====================================\n")
+	data := [][]string{{"Product", "ID/Name", "Status"}}
 	for _, resource := range resources {
-		fmt.Printf("%s - \033[32m%s\033[0m - %s - %s\n", resource.ProductName, resource.ResourceName, resource.ResourceID, resource.State)
+		data = append(data, []string{resource.ProductName, resource.ResourceName, resource.State.String()})
 	}
-	fmt.Printf("\nStatus: %d resources in total.Removed %d, In-Progress %d, Filtered %d\n", len(resources), resources.NumOf(types.Deleted), resources.NumOf(types.Removing), resources.NumOf(types.Filtered))
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.Header(data[0])
+	table.Bulk(data[1:])
+	table.Render()
+
+	/*
+		fmt.Print("\n====================================\n")
+		for _, resource := range resources {
+			fmt.Printf("%s - \033[32m%s\033[0m - %s - %s\n", resource.ProductName, resource.ResourceName, resource.ResourceID, resource.State)
+		}
+	*/
+	fmt.Printf("\nStatus: %d resources in total. Removed %d, In-Progress %d, Filtered %d\n", len(resources), resources.NumOf(types.Deleted), resources.NumOf(types.Removing), resources.NumOf(types.Filtered))
 }
